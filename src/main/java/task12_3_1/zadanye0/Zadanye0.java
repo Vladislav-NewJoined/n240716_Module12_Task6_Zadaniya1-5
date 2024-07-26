65eb269ababc4bf3c4d5fa468a2f5743ee5a6ec3
Vladislav-NewJoined <125866684+Vladislav-NewJoined@users.noreply.github.com>
2024-07-26 14:16:26 +0300

    240726 Initial commit

--- src/main/java/task12_3_1/zadanye0/Zadanye0.java
+++ src/main/java/task12_3_1/zadanye0/Zadanye0.java
@@ -2,7 +2,9 @@ package task12_3_1.zadanye0;
 
 import java.sql.*;
 
-// Команда в терминале для создания базы данных PostgreSQL: docker run --name postgresTest -d -p 5432:5432 -e POSTGRES_DB=somedbPGtest -e POSTGRES_USER=someuser -e POSTGRES_PASSWORD=123 postgres:alpine
+// Команда в терминале для создания контейнера mongo и базы данных MongoDB:
+// docker run --name mongoTest -d -p 27017:27017 mongo
+// URL: mongodb://localhost:27017/mongoTest
 // Значения параметров для настройки соединения в DBeaver (пришли в ответ на команду в терминале: docker inspect postgresTest
 // Сервер (Хост): 172.17.0.2 (нужно писать localhost вместо этого)
 // База данных: somedbPGtest
@@ -27,76 +29,90 @@ public class Zadanye0 {
                     'connect' в блоке 'DB Browser' внутри 'IntelliJ IDEA'
                     и обновите папку 'public' внутри базы данных):
             """);
+        System.out.println("""
+            В ответ на команды из терминала:
+            docker run --name mongoTest -d -p 27017:27017 mongo
+            docker inspect mong
+            Получена следующая информация:
+               "PATH=/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin",
+               "GOSU_VERSION=1.17",
+               "JSYAML_VERSION=3.13.1",
+               "MONGO_PACKAGE=mongodb-org",
+               "MONGO_REPO=repo.mongodb.org",
+               "MONGO_MAJOR=7.0",
+               "MONGO_VERSION=7.0.12",
+               "HOME=/data/db"
+        """);
 
-        connect();
-    }
+    connect();
+}
 
-    private static void connect() {
-        try (Connection connection = DriverManager.getConnection(URL, USER, PASSWORD)) {
-            try (Statement statement = connection.createStatement()) {
-                // Удаление таблицы 'users', если она уже существует
-                String dropTableQuery = "DROP TABLE IF EXISTS users";
-                statement.executeUpdate(dropTableQuery);
+private static void connect() {
+    try (Connection connection = DriverManager.getConnection(URL, USER, PASSWORD)) {
+        try (Statement statement = connection.createStatement()) {
+            // Удаление таблицы 'users', если она уже существует
+            String dropTableQuery = "DROP TABLE IF EXISTS users";
+            statement.executeUpdate(dropTableQuery);
 
-                // Создание таблицы 'users' с другой структурой
-                String createTableQuery = "CREATE TABLE users (" +
-                        "employee_id SERIAL PRIMARY KEY," +
-                        "first_name VARCHAR(20) NOT NULL," +
-                        "last_name VARCHAR(20) NOT NULL," +
-                        "email VARCHAR(20) NOT NULL," +
-                        "phone_number VARCHAR(20) NOT NULL," +
-                        "hire_date VARCHAR(20) NOT NULL," +
-                        "job_id VARCHAR(20) NOT NULL," +
-                        "salary DECIMAL(10,2) NOT NULL" +
-                        ")";
-                statement.executeUpdate(createTableQuery);
-                System.out.println("Таблица 'users' с новой структурой создана успешно.");
-            } catch (SQLException e) {
-                e.printStackTrace();
-            }
+            // Создание таблицы 'users' с другой структурой
+            String createTableQuery = "CREATE TABLE users (" +
+                    "employee_id SERIAL PRIMARY KEY," +
+                    "first_name VARCHAR(20) NOT NULL," +
+                    "last_name VARCHAR(20) NOT NULL," +
+                    "email VARCHAR(20) NOT NULL," +
+                    "phone_number VARCHAR(20) NOT NULL," +
+                    "hire_date VARCHAR(20) NOT NULL," +
+                    "job_id VARCHAR(20) NOT NULL," +
+                    "salary DECIMAL(10,2) NOT NULL" +
+                    ")";
+            statement.executeUpdate(createTableQuery);
+            System.out.println("Таблица 'users' с новой структурой создана успешно.");
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
 
-            try (Statement statement = connection.createStatement()) {
-                // Установка начального значения для столбца 'id' в таблице 'users'
-                String setInitialValueQuery = "ALTER SEQUENCE users_employee_id_seq RESTART WITH 100";
-                statement.executeUpdate(setInitialValueQuery);
-                System.out.println("Начальное значение столбца 'id' установлено на 100.");
-            } catch (SQLException e) {
-                e.printStackTrace();
-            }
+        try (Statement statement = connection.createStatement()) {
+            // Установка начального значения для столбца 'id' в таблице 'users'
+            String setInitialValueQuery = "ALTER SEQUENCE users_employee_id_seq RESTART WITH 100";
+            statement.executeUpdate(setInitialValueQuery);
+            System.out.println("Начальное значение столбца 'id' установлено на 100.");
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
 
-            try (Statement statement = connection.createStatement()) {
-                // Новая вставка данных в таблицу 'users'
-                String insertDataQuery = "INSERT INTO users (first_name, last_name, email, phone_number, hire_date, job_id, salary) VALUES " +
-                        "('Steben', 'King', 'SKING', '515.123.4567', '1987-06-17', 'AD_PRES', 24000.00), " +
-                        "('Neena', 'Kochhar', 'NKOCHHAR', '515.123.4568', '1987-06-18', 'AD_VP', 17000.00), " +
-                        "('Lex', 'De Haan', 'LDEHAAN', '515.123.4569', '1987-06-19', 'AD_VP', 17000.00), " +
-                        "('Alexander', 'Hunold', 'AHUNOLD', '590.423.4567', '1986-06-20', 'ID_PROG', 9000.00), " +
-                        "('Bruce', 'Ernst', 'BERNST', '590.423.4568', '1986-06-21', 'ID_PROG', 6000.00), " +
-                        "('David', 'Austin', 'DAUSTIN', '590.423.4569', '1986-06-22', 'ID_PROG', 4800.00), " +
-                        "('Valli', 'Pataballa', 'VPATABAL', '590.423.4569', '1986-06-23', 'ID_PROG', 4800.00)";
-                statement.executeUpdate(insertDataQuery);
-                System.out.println("Данные добавлены успешно.\n");
-            } catch (SQLException e) {
-                e.printStackTrace();
-            }
+        try (Statement statement = connection.createStatement()) {
+            // Новая вставка данных в таблицу 'users'
+            String insertDataQuery = "INSERT INTO users (first_name, last_name, email, phone_number, hire_date, job_id, salary) VALUES " +
+                    "('Steben', 'King', 'SKING', '515.123.4567', '1987-06-17', 'AD_PRES', 24000.00), " +
+                    "('Neena', 'Kochhar', 'NKOCHHAR', '515.123.4568', '1987-06-18', 'AD_VP', 17000.00), " +
+                    "('Lex', 'De Haan', 'LDEHAAN', '515.123.4569', '1987-06-19', 'AD_VP', 17000.00), " +
+                    "('Alexander', 'Hunold', 'AHUNOLD', '590.423.4567', '1986-06-20', 'ID_PROG', 9000.00), " +
+                    "('Bruce', 'Ernst', 'BERNST', '590.423.4568', '1986-06-21', 'ID_PROG', 6000.00), " +
+                    "('David', 'Austin', 'DAUSTIN', '590.423.4569', '1986-06-22', 'ID_PROG', 4800.00), " +
+                    "('Valli', 'Pataballa', 'VPATABAL', '590.423.4569', '1986-06-23', 'ID_PROG', 4800.00)";
+            statement.executeUpdate(insertDataQuery);
+            System.out.println("Данные добавлены успешно.\n");
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
 
-            try (Statement statement = connection.createStatement()) {
-                // Выполнение запроса на выборку всех имен и Employee ID
-                String selectNamesAndIdsQuery = "SELECT first_name, employee_id FROM users";
-                ResultSet namesAndIdsRs = statement.executeQuery(selectNamesAndIdsQuery);
+        try (Statement statement = connection.createStatement()) {
+            // Выполнение запроса на выборку всех имен и Employee ID
+            String selectNamesAndIdsQuery = "SELECT first_name, employee_id FROM users";
+            ResultSet namesAndIdsRs = statement.executeQuery(selectNamesAndIdsQuery);
 
-                while (namesAndIdsRs.next()) {
-                    int employeeId = namesAndIdsRs.getInt("employee_id");
-                    String firstName = namesAndIdsRs.getString("first_name");
-                    System.out.println("Employee ID: " + employeeId + ", First Name: " + firstName);
-                }
-
-            } catch (SQLException e) {
-                e.printStackTrace();
+            while (namesAndIdsRs.next()) {
+                int employeeId = namesAndIdsRs.getInt("employee_id");
+                String firstName = namesAndIdsRs.getString("first_name");
+                System.out.println("Employee ID: " + employeeId + ", First Name: " + firstName);
             }
 
         } catch (SQLException e) {
             e.printStackTrace();
         }
+
+    } catch (SQLException e) {
+        e.printStackTrace();
     }
+}
 }
