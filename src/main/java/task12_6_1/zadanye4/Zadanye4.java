360ce5d091e594fe44912af750a067de8b04fa76
Vladislav-NewJoined <125866684+Vladislav-NewJoined@users.noreply.github.com>
2024-07-29 16:28:27 +0300

    240729 Initial commit

---
+++ src/main/java/task12_6_1/zadanye4/Zadanye4.java
@@ -0,0 +1,20 @@
+package task12_6_1.zadanye4;
+
+public class Zadanye4 {
+    public static void main(String[] args) {
+        System.out.println("""
+            Модуль 12. Базы данных и Git. Задание №6:
+            Задание:
+            4. Разместите любой проект в данном репозитории
+            
+            Решение:
+            """);
+
+        System.out.println("""
+            Я создал репозиторий на ‘https://github.com/’ с названием ‘n240716_Module12_Task6_Zadaniya1-5_TestRep’
+            и индикатором доступа: ‘Public’.
+            Ссылка на этот репозиторий:
+            Я в данном репозитории разместил непосредственно этот проект.
+            """);
+    }
+}
