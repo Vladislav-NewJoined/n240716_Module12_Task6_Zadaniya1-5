7a2ec96cb41111420140a439c2de47da7c441c5c
Vladislav-NewJoined <125866684+Vladislav-NewJoined@users.noreply.github.com>
2024-07-29 11:58:44 +0300

    240729 commit

--- src/main/java/task12_5_1/zadanye4/Zadanye4.java
+++ src/main/java/task12_5_1/zadanye4/Zadanye4.java
@@ -7,6 +7,7 @@ import com.mongodb.client.MongoDatabase;
 import org.bson.Document;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 
 // Значения параметров для настройки соединения c сервером 'Mongo DB'
 // Сервер (Хост): localhost
@@ -43,27 +44,13 @@ public class Zadanye4 {
             collection.deleteMany(new Document());
 
             // Добавляем документы в коллекцию
-            Document doc1 = new Document("employee_id", 100).append("first_name", "Steben\t")
-                    .append("last_name", "King\t").append("email", "SKING\t").append("phone_number", "515.123.4567")
-                    .append("hire_date", "1987-06-17").append("job_id", "AD_PRES").append("salary", 24000.00);
-            Document doc2 = new Document("employee_id", 101).append("first_name", "Neena\t")
-                    .append("last_name", "Kochhar\t").append("email", "NKOCHHAR").append("phone_number", "515.123.4568")
-                    .append("hire_date", "1987-06-18").append("job_id", "AD_VP").append("salary", 17000.00);
-            Document doc3 = new Document("employee_id", 102).append("first_name", "Lex\t\t")
-                    .append("last_name", "De Haan\t").append("email", "LDEHAAN\t").append("phone_number", "515.123.4569")
-                    .append("hire_date", "1987-06-19").append("job_id", "AD_VP").append("salary", 17000.00);
-            Document doc4 = new Document("employee_id", 103).append("first_name", "Alexander")
-                    .append("last_name", "Hunold\t").append("email", "AHUNOLD\t").append("phone_number", "590.423.4567")
-                    .append("hire_date", "1986-06-20").append("job_id", "ID_PROG").append("salary", 9000.00);
-            Document doc5 = new Document("employee_id", 104).append("first_name", "Bruce\t")
-                    .append("last_name", "Ernst\t").append("email", "BERNST\t").append("phone_number", "590.423.4568")
-                    .append("hire_date", "1986-06-21").append("job_id", "ID_PROG").append("salary", 6000.00);
-            Document doc6 = new Document("employee_id", 105).append("first_name", "David\t")
-                    .append("last_name", "Austin\t").append("email", "DAUSTIN\t").append("phone_number", "590.423.4569")
-                    .append("hire_date", "1986-06-22").append("job_id", "ID_PROG").append("salary", 4800.00);
-            Document doc7 = new Document("employee_id", 106).append("first_name", "Valli\t")
-                    .append("last_name", "Pataballa").append("email", "VPATABAL").append("phone_number", "590.423.4569")
-                    .append("hire_date", "1986-06-23").append("job_id", "ID_PROG").append("salary", 4800.00);
+            Document doc1 = new Document("employee_id", 100).append("first_name", "Steben\t").append("last_name", "King\t").append("email", "SKING\t").append("phone_number", "515.123.4567").append("hire_date", "1987-06-17").append("job_id", "AD_PRES").append("salary", 24000.00);
+            Document doc2 = new Document("employee_id", 101).append("first_name", "Neena\t").append("last_name", "Kochhar\t").append("email", "NKOCHHAR").append("phone_number", "515.123.4568").append("hire_date", "1987-06-18").append("job_id", "AD_VP").append("salary", 17000.00);
+            Document doc3 = new Document("employee_id", 102).append("first_name", "Lex\t\t").append("last_name", "De Haan\t").append("email", "LDEHAAN\t").append("phone_number", "515.123.4569").append("hire_date", "1987-06-19").append("job_id", "AD_VP").append("salary", 17000.00);
+            Document doc4 = new Document("employee_id", 103).append("first_name", "Alexander").append("last_name", "Hunold\t").append("email", "AHUNOLD\t").append("phone_number", "590.423.4567").append("hire_date", "1986-06-20").append("job_id", "ID_PROG").append("salary", 9000.00);
+            Document doc5 = new Document("employee_id", 104).append("first_name", "Bruce\t").append("last_name", "Ernst\t").append("email", "BERNST\t").append("phone_number", "590.423.4568").append("hire_date", "1986-06-21").append("job_id", "ID_PROG").append("salary", 6000.00);
+            Document doc6 = new Document("employee_id", 105).append("first_name", "David\t").append("last_name", "Austin\t").append("email", "DAUSTIN\t").append("phone_number", "590.423.4569").append("hire_date", "1986-06-22").append("job_id", "ID_PROG").append("salary", 4800.00);
+            Document doc7 = new Document("employee_id", 106).append("first_name", "Valli\t").append("last_name", "Pataballa").append("email", "VPATABAL").append("phone_number", "590.423.4569").append("hire_date", "1986-06-23").append("job_id", "ID_PROG").append("salary", 4800.00);
             collection.insertOne(doc1);
             collection.insertOne(doc2);
             collection.insertOne(doc3);
@@ -74,22 +61,18 @@ public class Zadanye4 {
 
             System.out.println("Документы успешно добавлены в коллекцию\n");
 
-// Найдем всех работников и отсортируем их по убыванию заработной платы
-            FindIterable<Document> documents = collection.find()
-                    .sort(new Document("salary", -1)); // Сортировка по убыванию поля "salary"
+// Находим среднюю зарплату всех работников
+            Document averageSalaryDoc = collection.aggregate(
+                    Arrays.asList(
+                            new Document("$group", new Document("_id", null)
+                                    .append("avgSalary", new Document("$avg", "$salary")))
+                    )).first();
 
-// Печатаем заголовок
-            System.out.println("employee_id\t|\tfirst_name\t|\tlast_name\t|\tsalary");
-            System.out.println("--------------------------------------------------------");
+// Извлекаем значение средней зарплаты из результата агрегации
+            double averageSalary = averageSalaryDoc.getDouble("avgSalary"); // Устанавливаем значение по умолчанию, если нет данных
 
-// Печатаем данные с сортировкой
-            for (Document doc : documents) {
-                int employee_id = doc.getInteger("employee_id");
-                String first_name = doc.getString("first_name");
-                String last_name = doc.getString("last_name");
-                double salary = doc.getDouble("salary");
-                System.out.println(employee_id + "\t\t|\t" + first_name + "\t|\t" + last_name + "\t|\t" + salary);
-            }
+// Выводим результат
+            System.out.println("Средняя заработная плата сотрудников: " + averageSalary);
 
         } catch (Exception e) {
             System.err.println("Ошибка при работе с MongoDB: " + e.getMessage());
